538ce0419a43ffa9ea4369673efb1de5085f968c
marco987 <pacemarco87@gmail.com>
2019-07-30 16:08:34 +0200

    Handlebars, inseriti i segnaposto; AJAX, popolato l'array di oggetti

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+//Attraverso una chiamata ajax all’Api di boolean
+// avremo a disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+// https://flynn.boolean.careers/exercises/api/array/music
+
+
 $(document).ready(function() {
-	//Code
+
+  var dischiBoolean = "https://flynn.boolean.careers/exercises/api/array/music";
+
+  // Variabili handlebars
+  var source   = document.getElementById("template").innerHTML;
+  var template = Handlebars.compile(source);
+  var contenuto = [];
+
+  $.ajax(
+   {
+      url    : dischiBoolean,
+      method : "GET",
+      success: function(data){
+        // Memorizzo tutti i dischi
+        var dischi = data.response;
+        // Inserisco nell'array i dischi
+        contenuto.push(dischi)
+      },
+      error: function(){
+        alert('Problemi di server');
+      }
+    }
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
