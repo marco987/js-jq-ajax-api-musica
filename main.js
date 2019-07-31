de93599998d1afc7fcccfe07fe7bb3f83f2c36ff
marco987 <pacemarco87@gmail.com>
2019-07-31 10:09:43 +0200

    Commenti

--- main.js
+++ main.js
@@ -9,58 +9,41 @@ $(document).ready(function() {
 
   var dischiBoolean = "https://flynn.boolean.careers/exercises/api/array/music";
 
-  var contenuto = [];
-
   $.ajax(
    {
       url    : dischiBoolean,
       method : "GET",
+      // "Success", risposta positiva del server
       success: function(data){
-        // Memorizzo tutti i dischi
-        var dischi = data.response;
-        console.log(dischi);
+        // "Success", risposta positiva dell'API
+        if (data.success) {
+          // Memorizzo tutti i dischi
+          var dischi = data.response;
+          console.log(dischi);
 
-        for (var i = 0; i < dischi.length; i++) {
-          var disco = dischi[i];
-          console.log(disco);
+          for (var i = 0; i < dischi.length; i++) {
+            // Memorizzo il singolo disco
+            var disco = dischi[i];
+            console.log(disco);
 
-          var source   = $("#template").html();
-          var template = Handlebars.compile(source);
-          var context = {
-            poster: disco.poster,
-            title: disco.title,
-            author: disco.author,
-            year: disco.year
-          };
-          var html    = template(context);
+            var source   = $("#template").html();
+            var template = Handlebars.compile(source);
+            var context = {
+              poster: disco.poster,
+              title: disco.title,
+              author: disco.author,
+              year: disco.year
+            };
+            var html    = template(context);
 
-          $(".cds-container").append(html);
-
+            $(".cds-container").append(html);
+          }
         }
-
-
-
       },
       error: function(){
         alert('Problemi di server');
       }
     }
   );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
